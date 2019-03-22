9d44737027dc951667dd6f5400cf1a2065e473b9
plena <zoelam020@gmail.com>
2019-03-22 15:22:21 +0800

    add JSON Coder

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,46 +3,16 @@
 import Quick
 import Nimble
 import GSSwiftKit
+@testable import GSSwiftKit
 
-class TableOfContentsSpec: QuickSpec {
+class GSSwiftKitSpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
-
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
+        describe("StringChecker") {
             
             context("these will pass") {
 
                 it("can do maths") {
                     expect(23) == 23
-                }
-
-                it("can read") {
-                    expect("🐮") == "🐮"
-                }
-
-                it("will eventually pass") {
-                    var time = "passing"
-
-                    DispatchQueue.main.async {
-                        time = "done"
-                    }
-
-                    waitUntil { done in
-                        Thread.sleep(forTimeInterval: 0.5)
-                        expect(time) == "done"
-
-                        done()
-                    }
                 }
             }
         }
